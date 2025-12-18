85bfa9142626630800108bd786104c5d114bf86e
joaovitorsamora <jvsamora00@gmail.com>
2025-12-18 18:02:27 -0300

    update

--- GerenciadorDeTarefas/Enum/Enums.cs
+++ GerenciadorDeTarefas/Enum/Enums.cs
@@ -1,11 +1,14 @@
-﻿public enum Prioridade 
+﻿using System.Text.Json.Serialization;
+
+
+public enum Prioridade 
 { 
     Todas = 1,
     Alta = 2,
     Media = 3,
     Baixa = 4
 }
-
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum Status 
 { 
     Aberta = 1,
