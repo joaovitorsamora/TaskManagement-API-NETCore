6ab47d5cb56aa755951b41d52bd00cc755b6af74
joaovitorsamora <jvsamora00@gmail.com>
2025-12-19 09:43:23 -0300

    Ajuste de tipos de Status, Prioridade e DateTime das tarefas

---
+++ GerenciadorDeTarefas/Migrations/20251219124219_AjusteTiposTarefa.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GerenciadorDeTarefas.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteTiposTarefa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tarefas_Projetos_ProjetoId",
+                table: "Tarefas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjetoId",
+                table: "Tarefas",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DataCriacao",
+                table: "Tarefas",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tarefas_Projetos_ProjetoId",
+                table: "Tarefas",
+                column: "ProjetoId",
+                principalTable: "Projetos",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tarefas_Projetos_ProjetoId",
+                table: "Tarefas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjetoId",
+                table: "Tarefas",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DataCriacao",
+                table: "Tarefas",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tarefas_Projetos_ProjetoId",
+                table: "Tarefas",
+                column: "ProjetoId",
+                principalTable: "Projetos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
