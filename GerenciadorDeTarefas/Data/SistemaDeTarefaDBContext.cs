5a786875dc398f03944e5128e65459d756e3a720
joaovitorsamora <jvsamora00@gmail.com>
2025-12-19 11:00:16 -0300

    update

--- GerenciadorDeTarefas/Data/SistemaDeTarefaDBContext.cs
+++ GerenciadorDeTarefas/Data/SistemaDeTarefaDBContext.cs
@@ -1,20 +1,34 @@
 ﻿using GerenciadorDeTarefas.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
-using Npgsql.EntityFrameworkCore.PostgreSQL; 
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 namespace GerenciadorDeTarefas.Data
 {
     public class SistemaDeTarefaDBContext : DbContext
     {
-
         public DbSet<UsuarioModel> Usuarios { get; set; }
         public DbSet<ProjetoModel> Projetos { get; set; }
         public DbSet<TarefaModel> Tarefas { get; set; }
         public DbSet<TagModel> Tags { get; set; }
 
-        public SistemaDeTarefaDBContext(DbContextOptions<SistemaDeTarefaDBContext> options) : base(options) { }
-        
+        public SistemaDeTarefaDBContext(DbContextOptions<SistemaDeTarefaDBContext> options)
+            : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasPostgresEnum<Status>();
+            modelBuilder.HasPostgresEnum<Prioridade>();
+
+            
+            modelBuilder.Entity<TarefaModel>()
+                .Property(t => t.StatusTarefa)
+                .HasColumnType("status_enum");
+
+            modelBuilder.Entity<TarefaModel>()
+                .Property(t => t.PrioridadeTarefa)
+                .HasColumnType("prioridade_enum");
+        }
     }
 }
