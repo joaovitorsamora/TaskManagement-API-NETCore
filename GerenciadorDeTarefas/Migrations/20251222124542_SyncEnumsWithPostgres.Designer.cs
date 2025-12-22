777c92bfc152a6f0bd7332a14f183daa20e3ab76
joaovitorsamora <jvsamora00@gmail.com>
2025-12-22 09:48:18 -0300

    Refatora enums e sincroniza modelo com o banco
    Removidas definições de `prioridade_enum` e `status_enum` do
    modelo do banco de dados para simplificar o gerenciamento de
    enums. Ajustada a classe `TarefaModel` com a remoção das
    anotações `[Column(TypeName = ...)]` e alteração da propriedade
    `Projeto` para não virtual.
    
    Adicionada migração `SyncEnumsWithPostgres` para refletir as
    mudanças no banco de dados, garantindo a sincronização do
    modelo. Atualizado o arquivo `SyncEnumsWithPostgres.Designer.cs`
    para refletir o estado atualizado do modelo.
    
    Essas mudanças reduzem a complexidade e melhoram a
    compatibilidade do sistema.

--- GerenciadorDeTarefas/Migrations/20251222124542_SyncEnumsWithPostgres.Designer.cs
+++ GerenciadorDeTarefas/Migrations/20251222124542_SyncEnumsWithPostgres.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GerenciadorDeTarefas.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GerenciadorDeTarefas.Migrations
 {
     [DbContext(typeof(SistemaDeTarefaDBContext))]
-    partial class SistemaDeTarefaDBContextModelSnapshot : ModelSnapshot
+    [Migration("20251222124542_SyncEnumsWithPostgres")]
+    partial class SyncEnumsWithPostgres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- GerenciadorDeTarefas/Models/TarefaModel.cs
+++ GerenciadorDeTarefas/Models/TarefaModel.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace GerenciadorDeTarefas.Models
+﻿namespace GerenciadorDeTarefas.Models
 {
     public class TarefaModel
     {
@@ -8,14 +6,11 @@ namespace GerenciadorDeTarefas.Models
         public string? Titulo { get; set; }
         public DateTime DataCriacao { get; set; }
 
-        [Column(TypeName = "status_enum")]
         public Status StatusTarefa { get; set; }
-
-        [Column(TypeName = "prioridade_enum")]
         public Prioridade PrioridadeTarefa { get; set; }
 
-        public int? ProjetoId { get; set; } 
-        public virtual ProjetoModel? Projeto { get; set; }
+        public int? ProjetoId { get; set; }
+        public ProjetoModel? Projeto { get; set; }
 
         public int UsuarioId { get; set; }
         public UsuarioModel Usuario { get; set; } = null!;
